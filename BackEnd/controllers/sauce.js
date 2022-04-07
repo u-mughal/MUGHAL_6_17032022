356f5e7ab603939ffb232d554036497f80f71a51
Usman <usman.mughal@outlook.fr>
2022-04-07 11:18:03 +0200

    contrôleur like dislike et mise à zéro

--- BackEnd/controllers/sauce.js
+++ BackEnd/controllers/sauce.js
@@ -2,6 +2,7 @@
 const Sauce = require("../models/sauce");
 const fs = require("fs");
 
+
 /* Controleur creation sauce */
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
@@ -117,4 +118,97 @@ exports.deleteSauce = (req, res, next) => {
 
 /* Controleur like dislike */
 // Regle likeDislikeSauce : Like = 1 _ Dislike = -1 _ Pas de vote = 0
-exports.likeDislikeSauce = (req, res, next) => { };
+exports.likeDislikeSauce = (req, res, next) => {
+    let likeDislike = parseInt(req.body.like);
+    Sauce.findOne({
+        // id de la sauce
+        _id: req.params.id,
+    })
+        .then((sauce) => {
+            // Si sauce like = 1
+            if (likeDislike === 1) {
+                sauce.likes++;
+                // sauvegarde userId pour savoir si il a voté
+                sauce.usersLiked.push(req.body.userId);
+                // MAJ de la sauce avec données modifiées
+                Sauce.updateOne(
+                    { _id: req.params.id },
+                    {
+                        likes: sauce.likes,
+                        usersLiked: sauce.usersLiked,
+                        _id: req.params.id,
+                    }
+                )
+                    .then(() => res.status(200).json({ message: "Tu like ce produit !" }))
+                    .catch((error) => res.status(400).json({ error }));
+                // Si sauce dislike = -1
+            } else if (likeDislike === -1) {
+                sauce.dislikes++;
+                // sauvegarde userId pour savoir il si a voté
+                sauce.usersDisliked.push(req.body.userId);
+                // MAJ de la sauce avec données modifiées
+                Sauce.updateOne(
+                    { _id: req.params.id },
+                    {
+                        dislikes: sauce.dislikes,
+                        usersDisliked: sauce.usersDisliked,
+                        _id: req.params.id,
+                    }
+                )
+                    .then(() =>
+                        res.status(200).json({ message: "Tu dislike ce produit !" })
+                    )
+                    .catch((error) => res.status(400).json({ error }));
+                // verification et remise a zero sauce like et dislike
+            } else if (likeDislike === 0) {
+                // si userId est dans usersLiked = user like
+                if (sauce.usersLiked.includes(req.body.userId)) {
+                    // - 1 au like
+                    sauce.likes--;
+                    // userId est retirer du tableau = user ne like plus
+                    const index = sauce.usersLiked.indexOf(req.body.userId);
+                    sauce.usersLiked.splice(index, 1);
+                    // MAJ de la sauce avec données modifiées
+                    Sauce.updateOne(
+                        { _id: req.params.id },
+                        {
+                            likes: sauce.likes,
+                            usersLiked: sauce.usersLiked,
+                            _id: req.params.id,
+                        }
+                    )
+                        .then(() =>
+                            res.status(200).json({ message: "Tu ne like plus ce produit !" })
+                        )
+                        .catch((error) => res.status(400).json({ error }));
+                    // si userId est dans usersDisliked = user dislike
+                } else if (sauce.usersDisliked.includes(req.body.userId)) {
+                    // - 1 au dislike
+                    sauce.dislikes--;
+                    // userId est retirer du tableau = user ne dislike plus
+                    const index = sauce.usersDisliked.indexOf(req.body.userId);
+                    sauce.usersDisliked.splice(index, 1);
+                    // MAJ de la sauce avec données modifiées
+                    Sauce.updateOne(
+                        { _id: req.params.id },
+                        {
+                            dislikes: sauce.dislikes,
+                            usersDisliked: sauce.usersDisliked,
+                            _id: req.params.id,
+                        }
+                    )
+                        .then(() =>
+                            res
+                                .status(200)
+                                .json({ message: "Tu ne dislike plus ce produit !" })
+                        )
+                        .catch((error) => res.status(400).json({ error }));
+                }
+            }
+        })
+        .catch((error) => {
+            res.status(404).json({
+                error: error,
+            });
+        });
+};
