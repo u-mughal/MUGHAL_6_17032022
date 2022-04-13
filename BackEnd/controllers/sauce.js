8b84cbc8895456ee30c815e8a413ac01b68d887c
Usman <usman.mughal@outlook.fr>
2022-04-13 13:05:29 +0200

    comments + ajout de sécurité

--- BackEnd/controllers/sauce.js
+++ BackEnd/controllers/sauce.js
@@ -76,7 +76,7 @@ exports.modifySauce = (req, res, next) => {
                     };
                     // MAJ de la sauce avec données modifiées
                     Sauce.updateOne(
-                        { _id: req.params.id },
+                        { _id: req.params.id, userId: req.auth.userId },
                         { ...sauceObject, _id: req.params.id }
                     )
                         .then(() => res.status(200).json({ message: "Sauce mise à jour!" }))
@@ -88,7 +88,7 @@ exports.modifySauce = (req, res, next) => {
                 newItem.imageUrl = oldUrl;
                 // MAJ de la sauce avec données modifiées
                 Sauce.updateOne(
-                    { _id: req.params.id },
+                    { _id: req.params.id, userId: req.auth.userId },
                     { ...newItem, imageUrl: oldUrl, _id: req.params.id }
                 )
                     .then(() => res.status(200).json({ message: "Sauce mise à jour!" }))
@@ -102,8 +102,14 @@ exports.modifySauce = (req, res, next) => {
 /* Controleur suppression sauce */
 exports.deleteSauce = (req, res, next) => {
     // Recup sauce avec id
-    Sauce.findOne({ _id: req.params.id })
+    Sauce.findOne({ _id: req.params.id, userId: req.auth.userId })
         .then((sauce) => {
+            // Empêcher n'importe qu'elle utilisateur de delete une sauce
+            if (!sauce) {
+                res.status(404).json({
+                    error: new Error('No such Sauce!')
+                });
+            }
             // Extraction du nom du fichier à supprimer
             const filename = sauce.imageUrl.split("/images/")[1];
             fs.unlink(`images/${filename}`, () => {
